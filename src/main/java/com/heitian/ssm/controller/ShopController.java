274f4a8b379e130c866eb3b716948c6de660d7f7
ScaldingBlood <ScaldingBlood@gmail.com>
2016-11-28 12:41:23 +0800

    remove the useless files

--- src/main/java/com/heitian/ssm/controller/ShopController.java
+++ src/main/java/com/heitian/ssm/controller/ShopController.java
@@ -2,27 +2,38 @@ package com.heitian.ssm.controller;
 
 import com.heitian.ssm.model.Shop;
 import com.heitian.ssm.service.ShopService;
-import com.sun.deploy.net.HttpResponse;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by oasis on 11/27/16.
  */
-@RequestMapping("/service")
+@Controller
+@RequestMapping("/shop")
 public class ShopController {
     private final Logger log = Logger.getLogger(ShopController.class);
 
     @Autowired
     private ShopService shopService;
 
-    @RequestMapping(value = "{/register}", method = RequestMethod.POST)
-    public @ResponseBody Shop registerNewShop(HttpRequest request, HttpResponse response) {
-        
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public @ResponseBody String registerNewShop(@RequestParam Long ownerId,
+                                              @RequestParam String name,
+                                              @RequestParam String contact,
+                                              @RequestParam String email,
+                                              @RequestParam String telephone,
+                                              @RequestParam Long status) {
+        log.info("新店注册");
+        Shop shop = new Shop(ownerId, name, contact, email, telephone,status);
+        if(shopService.insertShop(shop)) {
+            log.info("注册成功");
+            return "success";
+        }
+        else {
+            log.info("注册失败");
+            return "error";
+        }
     }
 }
