cef70638d58518f9069f23ac0619c8556f886ab6
ScaldingBlood <ScaldingBlood@gmail.com>
2016-12-11 20:57:58 +0800

    add files

---
+++ src/main/java/com/heitian/ssm/dao/ShopAdDao.java
@@ -0,0 +1,7 @@
+package com.heitian.ssm.dao;
+
+/**
+ * Created by oasis on 12/11/16.
+ */
+public interface ShopAdDao {
+}
---
+++ src/main/java/com/heitian/ssm/model/ShopAd.java
@@ -0,0 +1,7 @@
+package com.heitian.ssm.model;
+
+/**
+ * Created by oasis on 12/11/16.
+ */
+public class ShopAd {
+}
