3a37592eb41260e8901ac8ed37346d1af14d4375
ScaldingBlood <ScaldingBlood@gmail.com>
2016-12-13 13:39:28 +0800

    some develop

---
+++ src/main/java/com/heitian/ssm/bo/OrderBo.java
@@ -0,0 +1,108 @@
+package com.heitian.ssm.bo;
+
+import java.util.Date;
+
+/**
+ * Created by oasis on 12/12/16.
+ */
+public class OrderBo {
+    private Long id;
+    private String customerEmail;
+    private String shopName;
+    private String ownerEmail;
+    private Long expressId;
+    private Long price;
+    private Long amount;
+    private Long commission;
+    private Long status;
+    private Long processStatus;
+    private Date createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public String getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public void setOwnerEmail(String ownerEmail) {
+        this.ownerEmail = ownerEmail;
+    }
+
+    public Long getExpressId() {
+        return expressId;
+    }
+
+    public void setExpressId(Long expressId) {
+        this.expressId = expressId;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public Long getCommission() {
+        return commission;
+    }
+
+    public void setCommission(Long commission) {
+        this.commission = commission;
+    }
+
+    public Long getStatus() {
+        return status;
+    }
+
+    public void setStatus(Long status) {
+        this.status = status;
+    }
+
+    public Long getProcessStatus() {
+        return processStatus;
+    }
+
+    public void setProcessStatus(Long processStatus) {
+        this.processStatus = processStatus;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
--- src/main/java/com/heitian/ssm/dao/OrderDao.java
+++ src/main/java/com/heitian/ssm/dao/OrderDao.java
@@ -3,6 +3,6 @@ package com.heitian.ssm.dao;
 /**
  * Created by oasis on 12/12/16.
  */
-public class OrderDao {
-    
+public interface OrderDao {
+
 }
