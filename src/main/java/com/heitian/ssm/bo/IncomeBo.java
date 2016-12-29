b8b65f128a399146fbf1c8300a30355a982124de
scaldingblood <scaldingblood@gmail.com>
2016-12-30 00:18:59 +0800

    fix commission and is_email_verified

--- src/main/java/com/heitian/ssm/bo/IncomeBo.java
+++ src/main/java/com/heitian/ssm/bo/IncomeBo.java
@@ -1,9 +1,12 @@
 package com.heitian.ssm.bo;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 /**
  * Created by unname on 2016/12/23.
  */
 public class IncomeBo extends TimeCondition{
+    @JsonSerialize(using=com.heitian.ssm.util.CustomDoubleSerializer.class)
     Double income;
 
     public Double getIncome() {
